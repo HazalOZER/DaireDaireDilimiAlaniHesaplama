53d1a7d14cca297f2ad906b720e2a55fbd80182b
Hazal <109552542+HazalOZER@users.noreply.github.com>
2022-09-18 16:09:13 +0300

    Update DaireAlan.java

--- src/DaireAlan.java
+++ src/DaireAlan.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class DaireAlan {
     public static void main(String[] args) {
+       
         double pi=3.14;
+      
         Scanner scan=new Scanner(System.in);
+       
         System.out.println("Lütfen Dairenin Yarıçapını yazınız");
         double r= scan.nextDouble();
+       
         double alan=pi*r*r;
+       
         System.out.println("Lütfen Hesaplanması İstenilen Daire Diliminin Merkez Açısını Giriniz");
         double dilimAcisi= scan.nextDouble();
+        
         double dilimAlanı= (pi*r*r*dilimAcisi)/360;
+       
         System.out.println("Dairenin Alanı: "+alan);
         System.out.println("Daire Diliminin Alanı: "+ dilimAlanı);
         System.out.println("Not: Pi 3,14 alınmıştır");
